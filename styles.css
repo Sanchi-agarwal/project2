09f377c204acd38a34e29a046f95e959c441fa3f
Sanchi-agarwal <sanchiagarwal06@gmail.com>
2025-08-04 19:33:58 +0530

    product landing page

---
+++ styles.css
@@ -0,0 +1,105 @@
+body {
+    font-family: 'Open Sans', sans-serif;
+    margin: 0;
+    padding: 0;
+    line-height: 1.6;
+    background-color: #f7f7f7;
+}
+
+header {
+    background-color: #333;
+    color: #fff;
+    padding: 1em;
+    text-align: center;
+}
+
+.header-title {
+    font-size: 36px;
+    margin-bottom: 10px;
+    font-weight: 700;
+}
+ 
+
+.product-description {
+    padding: 2em;
+    margin-bottom: 20px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    background-color: #fff;
+}
+
+.section-title {
+    font-size: 24px;
+    margin-bottom: 10px;
+    font-weight: 600;
+    background-color: rgb(240, 193, 131);
+}
+
+.description-text {
+    font-size: 18px;
+    margin-bottom: 20px;
+    background-color: rgb(253, 217, 169);
+    
+}
+
+.cta-button {
+    background-color: #000000;
+    color: #fff;
+    padding: 10px 20px;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    font-weight: 600;
+}
+
+.signup-form {
+    padding: 2em;
+    margin-bottom: 20px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    background-color: #fff;
+}
+
+.form-group {
+    margin-bottom: 20px;
+}
+
+label {
+    display: block;
+    margin-bottom: 10px;
+    font-weight: 600;
+}
+
+input[type="text"], input[type="email"] {
+    width: 70%;
+    height: 40px;
+    margin-bottom: 20px;
+    padding: 10px;
+    border: 1px solid #ccc;
+    font-size: 16px;
+}
+
+input[type="checkbox"] {
+    margin-bottom: 20px;
+}
+
+.submit-button {
+    background-color: #010101;
+    color: #fff;
+    padding: 10px 20px;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    font-weight: 600;
+}
+
+footer {
+    background-color: #333;
+    color: #fff;
+    padding: 1em;
+    text-align: center;
+    clear: both;
+}
+
+.footer-text {
+    font-size: 14px;
+    
+}
